86c02b7203168cdcc0dd29df1d801382007700c4
He Jiang <jiangherenren@163.com>
2023-01-11 19:15:55 +0800

    Add files via upload

---
+++ DeepModels/CSGISNet/demo_CSGIS.py
@@ -0,0 +1,103 @@
+import os
+import cv2
+import torch
+import os
+import numpy as np
+import argparse
+import math
+from torch.nn import MSELoss
+from defs import *
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--modelPath", default="./epoch 218_ssim 0.892429_psnr 28.471506")
+    parser.add_argument("--net", default="HDC_edge_refine")
+    parser.add_argument("--sessname", default="")
+    parser.add_argument("--test_dir", default="./data/")
+    parser.add_argument("--output_dir",default="./results/")
+    parser.add_argument("--if_crop",default = False)
+    return parser.parse_args()
+
+def cal_psnr(img1, img2):
+   mse = np.mean((img1/1.0 - img2/1.0) ** 2)
+   if mse < 1.0e-10:
+      return 100
+   return 10 * math.log10(1.0**2/mse)
+
+def get_image(image):
+    image = image*[255]
+    image = np.clip(image, 0, 255).astype(np.uint8)
+    return image
+
+def load_checkpoints(dir):
+    ckp_path = dir
+    try:
+        obj = torch.load(ckp_path)
+        print('Load checkpoint %s' % ckp_path)
+        return obj
+    except FileNotFoundError:
+        print('No checkpoint %s!!' % ckp_path)
+        return
+
+def run_test(input_dir, outout_dir, args):
+    with torch.no_grad():
+        ssim = SSIM().cuda()
+        mse = MSELoss().cuda()
+        if args.net == "HDC_edge_refine":
+            net = HDC_edge_refine(in_c=3, out_c=3, dim=64, num_block=20).cuda()
+        elif args.net == "PDP_edge_refine":
+            net = PDP_edge_refine(in_c=3, out_c=3, dim=64, num_block=12).cuda()
+        elif args.net == "Edge_guided":
+            net = Edge_guided(in_c=3, out_c=3, dim=64, num_block=12).cuda()
+        else:
+            print("NET name error!!")
+        net.eval()
+        obj = load_checkpoints(args.modelPath)
+        net.load_state_dict(obj['net'])
+
+
+        for image_name in os.listdir(input_dir):
+            image_file = os.path.join(input_dir, image_name)
+            print("Now is processing the image name " + image_name)
+            im = cv2.imread(image_file).astype("uint8")
+            image_o = (cv2.imread((image_file))/255.0).astype(np.float32)
+            h, w, c = image_o.shape
+            #
+            re_w = int(w/2)
+            #print(re_w) NKS no need
+            if args.if_crop:
+                image_o = image_o[0:h, re_w:w]
+            #
+            #print(image_o.shape)
+            #
+
+            image_o = np.transpose(image_o, (2, 0, 1))
+
+            image_o = torch.from_numpy(np.expand_dims(image_o, axis=0)).type(torch.FloatTensor).cuda()
+
+
+            if args.net == "HDC_edge_refine" or args.net == "PDP_edge_refine" or args.net =="Edge_guided":
+                edge, result, res = net(image_o)
+                result = result+res
+
+            else:
+                result = net(image_o)
+            result = result.cpu().detach().numpy()
+            result = np.transpose(result[0], (1, 2, 0))
+            result = get_image(result)
+            result  = np.clip(result, 0, 255).astype("uint8")
+            detail = im - result
+            detail_enhance = result + detail*2
+            detail_enhance = np.clip(detail_enhance, 0, 255).astype("uint8")
+            out_image_name = outout_dir + image_name[:-4] + '_CSGIS-Net2.png'
+            cv2.imwrite(out_image_name, detail_enhance)
+            print("........................................")
+
+if __name__ == '__main__':
+    args = get_args()
+
+    input_dir = args.test_dir
+    outout_dir = os.path.join(args.output_dir, args.sessname)
+    os.makedirs(outout_dir, exist_ok=True)
+    run_test(input_dir, outout_dir, args)
